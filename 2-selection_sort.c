d13b05565c05896ea14bccf346c6bdce7c466e21
jicruz96 <accounts@jicruz.com>
2020-06-17 21:24:20 +0000

    add 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,8 +1,32 @@
-#include "x.h"
+#include "sort.h"
 
 /**
- * function - description
- * @var: var description
- * Return: description
+ * selection_sort - sorts int array w/ selection sort, prints each step
+ * @array: pointer to array
+ * @size: array size
  **/
-void selection_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, tmp;
+	int *min;
+
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		min = array + i;
+
+		for (j = i + 1; j < size; j++)
+			if (*min > array[j])
+				min = array + j;
+
+		if (min != array + i)
+		{
+			tmp = array[i];
+			array[i] = *min;
+			*min = tmp;
+			print_array(array, size);
+		}
+	}
+}
